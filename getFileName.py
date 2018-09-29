fee74ee0866085f7b4ddd41793b4ae403d74ec4d
Administrator <Administrator@admin-PC>
2018-09-29 17:18:30 +0800

    生成tfrecord

--- getFileName.py
+++ getFileName.py
@@ -1,14 +1,15 @@
 import os
-def get_filename(dirname):
+def get_filename(dirname,subfixs):
 
-    result = []#所有的文件
+    result = []#鎵�鏈夌殑鏂囦欢
 
     for maindir, subdir, file_name_list in os.walk(dirname):
 
         for filename in file_name_list:
-            apath = os.path.join(maindir, filename)#合并成一个完整路径
-            result.append(apath)
+            if subfixs in filename:
+                apath = os.path.join(maindir+'/'+filename)#鍚堝苟鎴愪竴涓畬鏁磋矾寰�
+                result.append(apath)
 
     return result
 
-# print(get_filename("D:\Liang_Develop"))
+print(get_filename("E:/hellopython/object_detection/test_images",'.jpg'))
